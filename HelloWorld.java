43ca10d9ffb8bf976850e090f5fc8673a6782bfc
RidgeDev <studentjester42@gmail.com>
2014-09-09 17:53:52 +0100

    Hello World 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld{
   public static void main(String[] args)
   {
     //System.out.println("Hello World!");
-    JOptionPane.showMessageDialog(null, "Welcome to Java!");
+    JOptionPane.showMessageDialog(
+	null, 
+	"Welcome to Java!", 
+	"Hello World!",
+	JOptionPane.INFORMATION_MESSAGE);
   }
 }
